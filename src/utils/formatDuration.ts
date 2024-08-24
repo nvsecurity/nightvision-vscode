ea537f4e4f4f047d372dc273cda3d87080473b3d
Daniel Martins <daniel@nightvision.net>
2024-08-24 17:18:02 +0300

    [NV-2809] Round seconds in scan times

--- src/utils/formatDuration.ts
+++ src/utils/formatDuration.ts
@@ -1,7 +1,13 @@
 export default function formatDuration(duration: number) {
-  const secondsDuration = (duration >= 0 ? duration : 0) / 1000;
+  let secondsDuration = Math.round(Math.max(0, duration / 1000));
+
   const hours = Math.floor(secondsDuration / 3600);
-  const minutes = Math.floor((secondsDuration % 3600) / 60);
-  const seconds = Math.floor(secondsDuration % 60);
+  secondsDuration -= hours * 3600;
+  
+  const minutes = Math.floor(secondsDuration / 60);
+  secondsDuration -= minutes * 60;
+
+  const seconds = secondsDuration;
+
   return `${hours ? hours + ':' : ''}${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
 }
