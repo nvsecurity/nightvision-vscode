a29bfbfdca59f8f70db88b6eea6e0992f3df0f96
Daniel Martins <daniel@nightvision.net>
2024-08-25 03:06:12 +0300

    Update CLI_VERSION to 0.5.1

--- webpack/webview.config.js
+++ webpack/webview.config.js
@@ -56,7 +56,7 @@ const config = [
         process: 'process/browser',
       }),
       new webpack.DefinePlugin({
-        'process.env': { CLI_VERSION: JSON.stringify('0.5.0') },
+        'process.env': { CLI_VERSION: JSON.stringify('0.5.1') },
       }),
     ],
     devServer: {
