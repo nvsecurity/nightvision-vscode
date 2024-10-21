1c7566d915863c98e6cb11d3239fa7be54dfdf77
Daniel <149076889+dnighvn@users.noreply.github.com>
2024-10-21 15:25:07 +0300

    [NV-2720] Use selected project to apply filter if set (#37)

--- src/pages/Scans.tsx
+++ src/pages/Scans.tsx
@@ -1,3 +1,4 @@
+import { useProject } from '@hooks/useProject';
 import { useUser } from '@hooks/useUser';
 import { Project } from '@types_/project';
 import { ScanType, Severity, normalizedSeverity } from '@types_/scan';
@@ -10,6 +11,8 @@ import { messageHandler } from '@utils/MessageHandler';
 import formatDuration from '@utils/formatDuration';
 import { PageHeader } from '@components/PageHeader';
 import { API_URL } from '@constants/GlobalConstants';
+
+const ALL_PROJECTS_FILTER_OPTION: Project = { id: "<Internal-All>", name: "All" };
 
 const countIssues = (issues: any[]) => {
   return issues.reduce(
@@ -119,14 +122,15 @@ const getScans = async (
 };
 
 export const Scans = () => {
+  const { currentProject, setCurrentProject } = useProject();
   const { setIsLoggedIn } = useUser();
   const [scans, setScans] = useState<ScanType[]>();
   const [currentTime, setCurrentTime] = useState(new Date());
   const [projects, setProjects] = useState<Project[]>();
-  const [projectFilter, setProjectFilter] = useState<Project>();
+  const [projectFilter, setProjectFilter] = useState<Project>(currentProject);
 
   const filteredScans = scans?.filter(
-    (scan) => scan.project.id === projectFilter?.id || projectFilter?.id === ''
+    (scan) => scan.project.id === projectFilter?.id || projectFilter?.id === ALL_PROJECTS_FILTER_OPTION.id
   );
 
   useEffect(() => {
@@ -138,6 +142,12 @@ export const Scans = () => {
 
     return () => clearInterval(interval);
   }, [currentTime]);
+
+  useEffect(() => {
+    if (projectFilter && projectFilter?.id !== ALL_PROJECTS_FILTER_OPTION.id) {
+      setCurrentProject(projectFilter);
+    }
+  }, [projectFilter]);
 
   // Periodically get scans
   useEffect(() => {
@@ -272,7 +282,7 @@ export const Scans = () => {
             <div className='w-[calc(50%-.375rem)]'>
               <Dropdown
                 selectedItem={projectFilter}
-                items={[{ id: '', name: 'All' }, ...projects]}
+                items={[ALL_PROJECTS_FILTER_OPTION, ...projects]}
                 name='Project'
                 handleChange={setProjectFilter}
                 id='current-project'
