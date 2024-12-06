412168c539aa39f8dbc3ab0ec8fd25dde037cedf
Alesya Deschenya <alesya@nightvision.net>
2024-12-06 16:16:45 +0300

    upgrade CLI version to 0.7.2

--- webpack/webview.config.js
+++ webpack/webview.config.js
@@ -5,7 +5,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const dotenv = require('dotenv');
-const CLI_VERSION = '0.7.1';
+const CLI_VERSION = '0.7.2';
 
 dotenv.config();
 
