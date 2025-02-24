64155a69340172f3e03f222da42e2e924ddd4d6f
Daniel <149076889+dnighvn@users.noreply.github.com>
2025-02-24 18:35:51 +0200

    Upgrade CLI version to 0.8.1 (#78)

--- webpack/webview.config.js
+++ webpack/webview.config.js
@@ -5,7 +5,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const dotenv = require('dotenv');
-const CLI_VERSION = '0.8.0';
+const CLI_VERSION = '0.8.1';
 
 dotenv.config();
 
