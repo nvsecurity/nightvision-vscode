da1c9782129ac429c8b3acc90c55ed83e912c429
Daniel Martins <daniel@nightvision.net>
2025-03-12 21:20:36 +0200

    Upgrade CLI to 0.8.4

--- webpack/webview.config.js
+++ webpack/webview.config.js
@@ -5,7 +5,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const dotenv = require('dotenv');
-const CLI_VERSION = '0.8.3';
+const CLI_VERSION = '0.8.4';
 
 dotenv.config();
 
