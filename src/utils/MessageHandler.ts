76efc08ad4e0ad4b5d407c8e6e4fba5047969704
Daniel Martins <daniel@nightvision.net>
2025-04-08 06:46:27 +0300

    Reduce lagging when logging using the extension

--- src/utils/MessageHandler.ts
+++ src/utils/MessageHandler.ts
@@ -1,6 +1,6 @@
 import { v4 } from 'uuid';
 import { MessageData, Messenger } from '@utils/Messenger';
-import { API_ERROR_TYPES } from '@constants/GlobalConstants';
+import { API_ERROR_TYPES, HEALTH_CHECK_INTERVAL } from '@constants/GlobalConstants';
 import { CHECK_HEALTH, UNAUTHORIZED_ACCESS } from '@commands/CommandConstants';
 
 interface MessageHandlerApiProps {
@@ -14,6 +14,7 @@ class MessageHandler {
   private static instance: MessageHandler;
   private static listeners: { [commandId: string]: Function } = {};
   private callback: (message: MessageEvent<MessageData>) => void;
+  private lastTimeHealthCheck: number = 0;
 
   private constructor() {
     this.callback = (message: MessageEvent<MessageData>) => {
@@ -81,19 +82,23 @@ class MessageHandler {
         }
       };
 
-      // Check if CLI is alive
-      const result = messageHandler.requestGenerator(
-        CHECK_HEALTH,
-        v4(),
-      );
-      for await (const response of result) {
-        switch (response.command) {
-          case UNAUTHORIZED_ACCESS: {
-            setIsLoggedIn?.(false);
-            break;
+      const timeNow = new Date().getTime();
+      if (timeNow - this.lastTimeHealthCheck > HEALTH_CHECK_INTERVAL) {
+        // Check if CLI is alive
+        const result = messageHandler.requestGenerator(
+          CHECK_HEALTH,
+          v4(),
+        );
+        for await (const response of result) {
+          switch (response.command) {
+            case UNAUTHORIZED_ACCESS: {
+              setIsLoggedIn?.(false);
+              break;
+            }
           }
         }
       }
+      this.lastTimeHealthCheck = timeNow;
 
       const vscode = Messenger.getVsCodeAPI();
       vscode.postMessage({ url, method, body, requestId });
@@ -199,7 +204,13 @@ class MessageHandler {
       }
     );
 
-    yield* await iterator;
+    try {
+      yield* await iterator;
+    } finally {
+      // Prevent memory leaks by removing the listener when the generator is done
+      delete MessageHandler.listeners[requestId];
+    }
+    
   }
 }
 
