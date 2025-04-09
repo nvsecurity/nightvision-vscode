a7befb591070fadd914f3dc410ea52bf1badf1f5
Daniel Martins <daniel@nightvision.net>
2025-04-09 09:23:49 +0300

    Make HEALTH_CHECK_INTERVAL_SECONDS be 30

--- src/constants/GlobalConstants.ts
+++ src/constants/GlobalConstants.ts
@@ -5,7 +5,7 @@ export const API_URL = 'https://api.nightvision.net';
 export const API_ERROR_TYPES = ['client_error', 'validation_error', 'server_error'];
 export const CONTACT_EMAIL = 'support@nightvision.net';
 
-const HEALTH_CHECK_INTERVAL_SECONDS = 10;
+const HEALTH_CHECK_INTERVAL_SECONDS = 30;
 export const HEALTH_CHECK_INTERVAL = HEALTH_CHECK_INTERVAL_SECONDS * 1000;
 
 export const HTTPS_PARTS_REGEX = /^ht(t(ps?)?)?:?\/?\/?$/;
